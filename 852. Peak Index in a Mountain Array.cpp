071759b9e125815f0658550b6f5b6354fae8a666
Lalith Kiran Molleti <101722978+Lalith3470@users.noreply.github.com>
2023-04-27 18:22:21 +0530

    Create 852. Peak Index in a Mountain Array.cpp

---
+++ 852. Peak Index in a Mountain Array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int i=0,j=arr.size()-1;
+        while(i<j){
+            int m=(i+j)/2;
+            if(arr[m]>arr[m+1] && arr[m]>arr[m-1]) return m;
+            else if(arr[m]<arr[m+1] && arr[m]>arr[m-1]) i=m;
+            else j=m;
+        }
+        return 0;
+    }
+};
